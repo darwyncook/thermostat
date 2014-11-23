ea8d981d402b579c2f4df01a2b3acbeb39f2a5b5
darwyncook <cook@alfred.edu>
2014-11-23 12:53:23 -0500

    Created zones, fixed some bugs

--- zones.py
+++ zones.py
@@ -1,9 +1,11 @@
 __author__ = 'cook'
 from sensor import *
 
-
+# sensor is a string with the file name for the sensor from modprobe
+# zones_dict has sensor as a key, the value is a list with a zone desciption string and the GPIO pin the zone is
+# connected to.
 class Zone:
-    def __init(self, sensor, zones_dict):
+    def __init__(self, sensor, zones_dict):
         self.GPIO = zones_dict[sensor][1]
         self.switch = None
         self.temp = Temperature(sensor, zones_dict)
